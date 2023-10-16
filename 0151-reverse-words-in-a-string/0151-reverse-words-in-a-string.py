deb986a01cde44eefcf33902e221c02db1e4c211
nisrine <34342007+nisrinekane@users.noreply.github.com>
2023-10-16 16:58:15 -0700

    Time: 42 ms (44.81%), Space: 16.2 MB (90.69%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,6 +1,18 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        # second solution, testing for optimization         
+        #first solution
+        # mod_s = s.split()
+        # print(mod_s)
+        # left, right = 0, len(mod_s) - 1
+        # # double pointer loop
+        # while left <= right:
+        #     mod_s[left], mod_s[right] = mod_s[right], mod_s[left]
+        #     left += 1
+        #     right -= 1
+        # # removed manual trimming as split() handles that            
+        # return ' '.join(mod_s)
+        
+        # more optimized solution:        
         mod_s = s.split()
         mod_s = mod_s[::-1]  
         return ' '.join(mod_s)
