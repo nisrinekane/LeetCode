cead8209508eaa2f06d77cedc677d8452fec9745
nisrine <34342007+nisrinekane@users.noreply.github.com>
2023-10-26 17:04:43 -0700

    Time: 62 ms (75.95%), Space: 17.5 MB (54.15%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
-        for i in range(len(nums)):
-            difference = target - nums[i]
+        for n in range(len(nums)):
+#             calculate the amount needed to reach target
+            difference = target - nums[n]
+#     if the difference exists in sum
             if difference in seen:
-                return [i, seen[difference]]
-            else:
-                seen[nums[i]] = i
+#         return its index and the current index
+                return [seen[difference], n]
+#  add current number's index to dictionary
+            seen[nums[n]] = n
             
         
